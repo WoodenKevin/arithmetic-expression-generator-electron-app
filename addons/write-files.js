b67e60b939111512a397a82083afc0bf77de9040
WoodenKevin <WoodenKevin@foxmail.com>
2020-10-12 22:16:34 +0800

    fix: fix data exports

--- addons/write-files.js
+++ addons/write-files.js
@@ -1,16 +1,19 @@
 const fs = require('fs');
 const path = require('path');
+const process = require('process');
 
 let writeContentToFile = (filename, content) => {
-    let filepath = path.join(__dirname + `/${filename}`);
-
     try {
-        fs.writeFile(filepath, content, err => {
-            return {
-                status: 'error',
-                message: err,
-            };
-        });
+        fs.writeFile(
+            path.join(process.cwd() + `/${filename}`),
+            content,
+            err => {
+                return {
+                    status: 'error',
+                    message: err,
+                };
+            }
+        );
     } catch (err) {
         return {
             status: 'error',
